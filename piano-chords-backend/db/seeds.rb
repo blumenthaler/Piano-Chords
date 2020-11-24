11091640710f4119a906c13c22fb95b552331c28
blumenthaler <blumenthaleralex@gmail.com>
2020-11-24 15:17:02 -0500

    created seeds

--- piano-chords-backend/db/seeds.rb
+++ piano-chords-backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+major = Chord.create(name: "Major Triad", symbols: "CM, C", structure: "4, 3")
+minor = Chord.create(name: "Minor Triad", symbols: "Cm, C-", structure: "3, 4")
+dim = Chord.create(name: "Diminished Triad", symbols: "Cmb5, Co", structure: "3, 3")
+aug = Chord.create(name: "Augmented Triad", symbols: "C#5, C+", structure: "4, 4")
