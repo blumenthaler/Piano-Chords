c083ff267195e6cb141b255b8f194b269053560a
blumenthaler <blumenthaleralex@gmail.com>
2020-11-26 11:59:31 -0500

    debugged structure breakdown; all chord types work

--- piano-chords-backend/db/seeds.rb
+++ piano-chords-backend/db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-major = Chord.create(name: "Major Triad", symbols: "M, ", structure: "4, 3")
-minor = Chord.create(name: "Minor Triad", symbols: "m, -", structure: "3, 4")
-dim = Chord.create(name: "Diminished Triad", symbols: "mb5, o", structure: "3, 3")
-aug = Chord.create(name: "Augmented Triad", symbols: "#5, +", structure: "4, 4")
+major = Chord.create(name: "Major Triad", symbols: "M, ", structure: "0, 4, 7")
+minor = Chord.create(name: "Minor Triad", symbols: "m, -", structure: "0, 3, 7")
+dim = Chord.create(name: "Diminished Triad", symbols: "mb5, o", structure: "0, 3, 6")
+aug = Chord.create(name: "Augmented Triad", symbols: "#5, +", structure: "0, 4, 8")
 
-maj7 = Chord.create(name: "Major Seven", symbols: "M7, Δ7", structure: "4, 3, 4")
-min7 = Chord.create(name: "Minor Seven", symbols: "m7, -7", structure: "3, 4, 3")
-half_dim = Chord.create(name:"Half Diminished Seven", symbols: "ø7, m7b5", structure: "3, 3, 4")
-whole_dim = Chord.create(name:"Diminished Seven", symbols: "o7", structure: "3, 3, 3")
+maj7 = Chord.create(name: "Major Seven", symbols: "M7, Δ7", structure: "0, 4, 7, 11")
+min7 = Chord.create(name: "Minor Seven", symbols: "m7, -7", structure: "0, 3, 7, 10")
+half_dim = Chord.create(name:"Half Diminished Seven", symbols: "ø7, m7b5", structure: "0, 3, 6, 10")
+whole_dim = Chord.create(name:"Diminished Seven", symbols: "o7", structure: "0, 3, 6, 9")
 
-maj9 = Chord.create(name: "Major Nine", symbols: "M9, Δ9", structure: "4, 3, 4, 3")
+maj9 = Chord.create(name: "Major Nine", symbols: "M9, Δ9", structure: "0, 4, 7, 11, 14")
