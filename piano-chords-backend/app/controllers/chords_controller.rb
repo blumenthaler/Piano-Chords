51f0131734185a120523ca11b0521686579ab6ea
blumenthaler <blumenthaleralex@gmail.com>
2020-11-27 16:39:21 -0500

    defined chords#create action

--- piano-chords-backend/app/controllers/chords_controller.rb
+++ piano-chords-backend/app/controllers/chords_controller.rb
@@ -6,4 +6,14 @@ class ChordsController < ApplicationController
         # }
         render json: ChordSerializer.new(chords)#, options)
     end
+
+    def create
+        chord = Chord.create(
+            name: params[:name]
+            structure: params[:structure]
+            symbols: params[:symbols]
+            user: User.find(params[:user_id])
+        )
+        render json: chord
+    end
 end
