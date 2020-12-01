245ac28eed7cf61f4556428c3f9272cca55e256b
blumenthaler <blumenthaleralex@gmail.com>
2020-12-01 10:16:18 -0500

    removed unused routes/controller actions

--- piano-chords-backend/app/controllers/users_controller.rb
+++ piano-chords-backend/app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-    def index
-        users = User.all
-        options = {
-            include: [:chords]
-        }
-        render json: UserSerializer.new(users, options)
-    end
-
     def create
         user = User.create(
             username: params[:username]
